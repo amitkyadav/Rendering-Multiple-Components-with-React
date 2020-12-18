c4a477ae4ed8b43b78fcd0ed04dbce6cc58cebf7
amitkyadav <38914912+amitkyadav@users.noreply.github.com>
2020-12-18 11:19:59 +0530

    rendering-multiple-components-with-react

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div>
+      <p>legleRule app</p>
+      <p>varifing all document online no need to cary hard copy of document</p>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App data-ns-test="project-name" />,
+  document.getElementById("root")
+);
+ReactDOM.render(
+  <App data-ns-test="project-descripttion" />,
+  document.getElementById("root")
+);
