fb3983b30890d3a757874608052db95d88c299a1
amitkyadav <38914912+amitkyadav@users.noreply.github.com>
2020-12-27 12:41:32 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1 data-ns-test="project-name">legleRule app</h1>
-      <p data-ns-test="project-descripttion">
+      <p data-ns-test="project-name">
         varifing all document online no need to cary hard copy of document
       </p>
     </div>
