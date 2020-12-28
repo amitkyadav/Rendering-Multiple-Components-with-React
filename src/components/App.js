81b0f50ffbface8703e3400122b7f336e65e0597
amitkyadav <38914912+amitkyadav@users.noreply.github.com>
2020-12-28 09:42:57 +0530

    added id for div

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 
 const App = () => {
   return (
-    <div>
+    <div id="main">
       <h1 data-ns-test="project-name">xyz</h1>
       <p data-ns-test="project-descripttion">this is description</p>
     </div>
